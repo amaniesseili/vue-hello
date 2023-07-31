c6f23d61b92fef525d8333f8ed76cdfa999d7703
amani esseili <esseiliamani@hotmail.com>
2023-07-31 19:15:20 +0200

    vue hello completed

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp ({
+    data(){
+        data: {
+            
+            return {
+                messaggio : "Benvenuto nel nostro sito",
+                imageUrl :  "img/vue-img.png",
+            }
+        }
+        
+    }
+})
+
+app.mount("#app");
